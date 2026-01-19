8fe93106383a2e670d3e7dc9b5e36bccc2f56e61
Josh Nebreja <Josh-L30>
2026-01-19 23:08:02 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+
+const studentRoutes = require("./routes/student.routes");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/students", studentRoutes);
+
+app.get("/", (req, res) => {
+    res.send("API is running...");
+});
+
+module.exports = app;
